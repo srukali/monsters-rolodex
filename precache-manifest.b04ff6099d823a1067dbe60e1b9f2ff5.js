07963826a3fd842d006e8e7f3ccba5d8ce2b8955
Katelyn Smith <katelyn@agentology.com>
2019-10-23 11:20:35 -0700

    Updates

---
+++ precache-manifest.b04ff6099d823a1067dbe60e1b9f2ff5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "741c8daefe787abc5182b4e23cabcac6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ee5efba57f2e2d9df26a",
+    "url": "/monsters-rolodex/static/css/main.27fdcfea.chunk.css"
+  },
+  {
+    "revision": "f977fa80ff73af86627e",
+    "url": "/monsters-rolodex/static/js/2.37e904de.chunk.js"
+  },
+  {
+    "revision": "ee5efba57f2e2d9df26a",
+    "url": "/monsters-rolodex/static/js/main.d7e32a0c.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
